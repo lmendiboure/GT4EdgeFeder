dcfe7601f42fe18855194007d36511d3fc57c3c1
lmendiboure <leo.mendiboure11@gmail.com>
2024-04-30 10:58:27 +0200

    Update functions.py -> Add solution to compute storage utilization

--- functions/functions.py
+++ functions/functions.py
@@ -4,7 +4,8 @@ import yaml
 import time
 from kubernetes import client, config, watch
 from datetime import datetime
-from functions.utils import load_config, convert_memory_usage_to_megabytes, parse_memory_string, write_to_csv        
+from functions.utils import load_config, convert_memory_usage_to_megabytes, parse_memory_string, write_to_csv    
+import psutil    
 
 def watch_pods():
     """
@@ -54,105 +55,122 @@ def watch_pods():
     except Exception as e:
         # Handle any exceptions and print error message
         print(f"Error watching pods:", e)
+        
+        
 
+def get_cpu_usage_percent():
+    """Get the CPU usage percentage."""
+    return psutil.cpu_percent(interval=1)
 
-def get_storage_and_resource_utilization_for_all_nodes(output_file=None):
-    """
-    Get storage usage, CPU, and RAM utilization for all nodes in the Kubernetes cluster. Write results in a CSV file format IF file given. Otherwise, it is sent back as data.
-    """
+def get_ram_usage_percent():
+    """Get the RAM usage percentage."""
+    return psutil.virtual_memory().percent
+
+def get_cluster_nodes_usage():
+    """Get CPU and RAM usage for all nodes in the Kubernetes cluster."""
+    config.load_kube_config()  
+
+    v1 = client.CoreV1Api()
+    nodes = v1.list_node().items
+
+    node_usage = {}
+
+    for node in nodes:
+        node_name = node.metadata.name
+        node_ip = node.status.addresses[0].address
+        node_cpu_usage = get_cpu_usage_percent()
+        node_ram_usage = get_ram_usage_percent()
+        node_usage[node_name] = {
+            'ip': node_ip,
+            'cpu_usage_percent': node_cpu_usage,
+            'ram_usage_percent': node_ram_usage
+        }
+
+    return node_usage
+
+def get_ephemeral_storage_usage(namespace, storage_size, pod_name, pod_storage):
+    """Get the ephemeral storage usage for all nodes in the Kubernetes cluster."""
     try:
+        config.load_kube_config()  
 
+        api_instance = client.CoreV1Api()
+        nodes = api_instance.list_node().items
+
+        storage_data = {}
+
+        for node in nodes:
+            node_name = node.metadata.name
+            total_storage_used = 0
+
+            pods = api_instance.list_namespaced_pod(namespace=namespace, field_selector=f"spec.nodeName={node_name}").items
+
+            for pod in pods:
+                if pod.status.phase == "Running":
+                    total_storage_used += pod_storage
+
+            storage_data[node_name] = total_storage_used/storage_size
+
+        return storage_data
+
+    except Exception as e:
+        print(f"Error retrieving ephemeral storage utilization information:", e)
+        return None
+
+def get_pod_storage_usage(api_instance, namespace, pod_name):
+    """Get the storage usage of a specific pod."""
+    try:
+        pod = api_instance.read_namespaced_pod_status(name=pod_name, namespace=namespace)
+        total_storage_used = 0
+
+        for container in pod.spec.containers:
+            for volume_mount in container.volume_mounts:
+                if volume_mount.mount_path == "/var/lib/docker":
+                    total_storage_used += volume_mount.size_limit
+
+        return total_storage_used
+
+    except Exception as e:
+        print(f"Error retrieving storage usage for pod {pod_name}:", e)
+        return 0
+
+def get_nodes_utilization(output_file=None):
+    """Get CPU, RAM, and ephemeral storage utilization for all nodes in the Kubernetes cluster."""
+    try:
+    
+    	# Retrieve infos from config file
         config_data = load_config("config.yaml")
         
         namespace = config_data['namespace']
-        disk_size_gb = config_data.get('disk_size', 0)  # Disk size in GB
-        nodes_number = config_data.get('nodes_number', 1)  # Number of nodes
-        memory_per_node_mb = config_data.get('memory', 0)  # Memory per node in MB
-        cpus_per_node = config_data.get('cpus', 0)  # CPUs per node
         
-        results_file_nodes = config_data.get('results_file_nodes', 'data.csv') if output_file is None else output_file  # File to load results
+        disk_size = config_data['disk_size']
+        
+        nodes_number = config_data['nodes_number']
+        
+        base_storage_per_node_gb = disk_size / nodes_number
+       
+        pod_name_prefix = config_data['pods_config'][0]['name'] 
+        
+        pod_storage = config_data['pods_config'][0]['storage']      
+        
+        results_file_nodes = config_data.get('results_file_nodes', 'data.csv') if output_file is None else output_file
 
-        # Calculate base storage capacity per node
-        base_storage_per_node_gb = disk_size_gb / nodes_number
+	# Compute CPU and RAM usage
+        node_usage = get_cluster_nodes_usage()
+        
+        # Compute storage usage
+        storage_data = get_ephemeral_storage_usage(namespace, base_storage_per_node_gb, pod_name_prefix, pod_storage)
 
-        # Load Kubernetes configuration
-        config.load_kube_config()
-
-        # Create a client to interact with the Kubernetes API
-        api_instance = client.CoreV1Api()
-
-        # Get the list of nodes in the cluster
-        nodes = api_instance.list_node().items
-
-        # Data list to store the data for CSV
-        data = []
-
-        # Get the timestamp once
         timestamp = datetime.now()
 
-        # Iterate over each node in the cluster
-        for node in nodes:
-            node_name = node.metadata.name
-            total_memory_usage_bytes = 0
-            total_cpu_usage_millicores = 0
-
-            # Get the list of pods running on the current node
-            pods = api_instance.list_namespaced_pod(namespace=namespace, field_selector=f"spec.nodeName={node_name}").items
-
-            # Iterate over each pod and retrieve resource usage
-            for pod in pods:
-                if pod.status.phase == "Running":  # Only consider pods in "Running" phase
-                    for container in pod.spec.containers:
-                        # Memory usage
-                        memory_request = container.resources.requests.get("memory", "0Ki")
-                        memory_request_bytes = parse_memory_string(memory_request)
-                        total_memory_usage_bytes += memory_request_bytes
-
-                        # CPU usage
-                        cpu_request = container.resources.requests.get("cpu", "0")
-                        cpu_request_millicores = int(cpu_request[:-1]) if cpu_request.endswith("m") else int(cpu_request)
-                        total_cpu_usage_millicores += cpu_request_millicores
-
-            # Convert memory usage to megabytes
-            total_memory_usage_mb = convert_memory_usage_to_megabytes(total_memory_usage_bytes)
-
-            # Calculate memory utilization rate
-            memory_utilization_rate = (total_memory_usage_mb / memory_per_node_mb) * 100
-
-            # Calculate CPU utilization rate
-            cpu_utilization_rate = (total_cpu_usage_millicores / (cpus_per_node * 1000)) * 100
-
-            # Get storage usage for pods on the node
-            total_storage_used = 0
-            for pod_config in config_data.get('pods_config', []):
-                pod_name_prefix = pod_config['name']
-                field_selector = f"spec.nodeName={node_name}"
-                pods = api_instance.list_namespaced_pod(namespace, field_selector=field_selector).items
-
-                # Calculate total storage used by pods corresponding to the current node
-                for pod in pods:
-                    if pod.status.phase == "Running":  # Only consider pods in "Running" phase	
-                        if pod.metadata.name.startswith(pod_name_prefix):
-                            pod_storage = pod_config['storage']
-                            total_storage_used += pod_storage
-
-            # Calculate storage related values
-            storage_used_percent = (total_storage_used / base_storage_per_node_gb) * 100
-            storage_available_percent = 100 - storage_used_percent
-
-            # Append data to the list
-            data.append((node_name, cpu_utilization_rate, memory_utilization_rate, storage_used_percent))
-
-        # Write data to CSV with the same timestamp for all nodes
-        if output_file:
-            for node_data in data:
-                write_to_csv(results_file_nodes, (timestamp,) + node_data)
-        else:
-            return data
+        if node_usage:
+            for node_name, node_data in node_usage.items():
+                cpu_utilization_rate = node_data['cpu_usage_percent']
+                ram_utilization_rate = node_data['ram_usage_percent']
+                storage_usage = storage_data.get(node_name, 0)
+                write_to_csv(results_file_nodes, (timestamp, node_name, cpu_utilization_rate, ram_utilization_rate, storage_usage))
 
     except Exception as e:
         print(f"Error retrieving resource utilization and storage information:", e)
-
 
 def watch_nodes():
     """
@@ -161,5 +179,5 @@ def watch_nodes():
     config_data = load_config("config.yaml")
     results_file_nodes = config_data.get('results_file_nodes', 'data.csv') 
     while True:
-        get_storage_and_resource_utilization_for_all_nodes(output_file=results_file_nodes)
+        get_nodes_utilization(output_file=results_file_nodes)
         time.sleep(1)
