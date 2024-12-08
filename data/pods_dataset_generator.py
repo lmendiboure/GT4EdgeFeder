30f6e3783f732887340f9ad2146697285bebd808
lmendiboure <leo.mendiboure11@gmail.com>
2024-12-08 18:27:48 +0100

    correcting pod dataset generation MB-> Mb

--- data/pods_dataset_generator.py
+++ data/pods_dataset_generator.py
@@ -1,4 +1,5 @@
 import random
+import argparse
 import math
 import sys
 sys.path.append('../functions')
@@ -23,14 +24,32 @@ def get_ran_infos_for_pods(config_data,pod_config):
     data = random_line.strip().split(',')
     traffic_load = int(data[11])
     latency = int(data[12])
-    return math.ceil((pod_config["transmission_uplink"]+pod_config["transmission_downlink"])/(traffic_load*1000)+latency)
+    print(latency)
+    return math.ceil((pod_config["transmission_uplink"]+pod_config["transmission_downlink"])/(traffic_load*1000/(8*1000))+latency)
 
+# Function to compute weights depending on the selected mode
+def calculate_weights(nodes_number, mode):
+    if mode == "uniform":
+        # Tous les nœuds ont le même poids
+        return [1 / nodes_number] * nodes_number
+    elif mode == "mid_high":
+        # 60% pour le premier nœud, 40% répartis équitablement entre les autres
+        weights = [0.6] + [0.4 / (nodes_number - 1)] * (nodes_number - 1)
+        return weights
+    elif mode == "high":
+        # 80% pour le premier nœud, 20% répartis équitablement entre les autres
+        weights = [0.8] + [0.2 / (nodes_number - 1)] * (nodes_number - 1)
+        return weights
+    else:
+        raise ValueError("Invalid mode. Choose 'uniform', 'mid_high', or 'high'.")
+        
+        
     
 # Function to generate pods dataset for a specific duration and a variable number of pods
     
-def generate_pods_dataset():
+def generate_pods_dataset(mode):
     
-    config_data = load_config("config.yaml")
+    config_data = load_config("temp-config.yaml")
 
     random.seed(config_data["reproductible_seed"]) # Initialize PRNG for reproductibility purposes
     
@@ -58,7 +77,18 @@ def generate_pods_dataset():
             
             pod_name = f"{pod_config['name']}-{pod_type[0]}{pod_type[1]}-{pod_counter}" # Unique pod name
             
-            node_name = config_data["nodes_config"][(random.randint(0, config_data["nodes_number"]-1))]["id"] # Random node to attach pod
+            nodes_number = config_data["nodes_number"]
+           
+            weights = calculate_weights(nodes_number, mode)
+            
+            nodes = config_data["nodes_config"][:nodes_number]
+           
+
+            node_name = random.choices(population=[node["id"] for node in nodes], weights=weights, k=1)[0]
+            
+            print(node_name)
+           
+            #node_name = config_data["nodes_config"][(random.randint(0, config_data["nodes_number"]-1))]["id"] # Random node to attach pod
             
             ran_delay=get_ran_infos_for_pods(config_data,pod_config) # Compute transmission delay
             
@@ -71,6 +101,16 @@ def generate_pods_dataset():
         	
         time += config_data["pods_interval"]
 
-# generate dataset
+if __name__ == "__main__":
+    # Check argus
+    parser = argparse.ArgumentParser(description="Indicate weighted probabilities.")
+    parser.add_argument("mode", choices=["uniform", "mid_high", "high"], help="Weighting mode.")
+    args = parser.parse_args()
 
-generate_pods_dataset()
+    # generate dataset
+    generate_pods_dataset(args.mode)
+
+
+
+
+
